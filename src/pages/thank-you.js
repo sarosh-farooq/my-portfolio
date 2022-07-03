5a9cc5ab0593d99f978fdd58178d50c01f31f210
Sarosh Farooq <88868910+sarosh-farooq@users.noreply.github.com>
2022-07-03 21:36:58 +0500

    create thank you

---
+++ src/pages/thank-you.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from "gatsby"
+const Thankyou = () => {
+    return (
+        <>
+            <div>Thankyou</div>
+            <Link to="/">Go home</Link>.
+        </>
+    )
+}
+
+export default Thankyou
